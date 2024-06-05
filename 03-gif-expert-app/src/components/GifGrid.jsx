c56e36c37a408a379f64e86d5dba04e42ca85816
ezezapata <zapata.ezequiel@gmail.com>
2024-06-05 18:18:30 -0300

    actualización

--- 03-gif-expert-app/src/components/GifGrid.jsx
+++ 03-gif-expert-app/src/components/GifGrid.jsx
@@ -1,6 +1,35 @@
-export const GifGrid = ( {category} ) => 
-    (
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
+
+export const GifGrid = ({ category }) => {
+
+    const [ images, setImages ] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGifs( category );
+        setImages( newImages );
+    }
+
+    useEffect( () => {
+        getImages();
+    }, [] );
+    // Nunca NUNCA hay que poner la ejecución de una función en un functional component
+    // getGifs( category );
+
+    return (
         <>
-            <p>{ category }</p>
+            <h3>{ category }</h3>
+            <div className="card-grid">
+                { images.map( img => (
+                    // <li key={ img.id }>{ img.title }</li>
+                    <GifItem  
+                        key={ img.id }  
+                        { ...img } />
+                    ))
+                }
+            </div>
         </>
     );
+
+}
