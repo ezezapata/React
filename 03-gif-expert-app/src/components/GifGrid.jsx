e0b7a7fcf861812b038d01497cdc7099d01bc6fc
ezezapata <zapata.ezequiel@gmail.com>
2024-06-09 15:22:04 -0300

    Preparar para deploy

--- 03-gif-expert-app/src/components/GifGrid.jsx
+++ 03-gif-expert-app/src/components/GifGrid.jsx
@@ -4,8 +4,6 @@ import { useFetchGifs } from "../hooks/useFetchGifs";
 export const GifGrid = ({ category }) => {
 
     const { images, isLoading } = useFetchGifs( category );
-
-    console.log( isLoading );
     
     return (
         <>
